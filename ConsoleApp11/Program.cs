800642678eeb4ea26d8dd22407a13fa0b3289226
Ali Onur Puskullu <puskullualionur@gmail.com>
2022-06-21 10:24:46 +0300

    Operator Ornegi

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -2,11 +2,24 @@
 
 namespace ConsoleApp11
 {
+    class result
+    {
+        public static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
+
+            int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+            result.solve(meal_cost, tip_percent, tax_percent);
         }
     }
 }
